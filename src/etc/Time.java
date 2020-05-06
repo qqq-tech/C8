d35115df99e32a826726fddbc4bc4063948f89e3
developer.ggyu <developer.ggyu@gmail.com>
2020-05-06 23:06:11 +0900

    time 수정

--- src/etc/Time.java
+++ src/etc/Time.java
@@ -2,13 +2,19 @@ package etc;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class Time {
-	
-	public static Date getStringToTime(String time)
-	{
+	public static Date addTime(Date date,int sec) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.SECOND, sec);
+		return cal.getTime();
+	}
+
+	public static Date getStringToTime(String time) {
 		SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss", Locale.KOREA);
 		Date d1 = null;
 		try {
@@ -20,8 +26,14 @@ public class Time {
 		return d1;
 	}
 	
-	public static long getDiffTime(Date d1, Date d2)
-	{
+	public static String getTimeToString(Date time) {
+		//yyyy-MM-dd HH:mm:ss
+		SimpleDateFormat transFormat = new SimpleDateFormat("HH:mm:ss");
+		String to = transFormat.format(time);
+		return to;
+	}
+
+	public static long getDiffTime(Date d1, Date d2) {
 		long diff = d1.getTime() - d2.getTime();
 		long sec = diff / 1000;
 		return sec;
