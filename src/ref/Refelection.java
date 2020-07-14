81bbb98555c7609f37e04f306673557e2af28f28
developer.ggyu <developer.ggyu@gmail.com>
2020-07-14 21:55:36 +0900

    add reflection function

--- src/ref/Refelection.java
+++ src/ref/Refelection.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class Refelection extends ClassLoader {
 	/*
@@ -68,6 +69,33 @@ public class Refelection extends ClassLoader {
 		return "aaa";
 	}
 
+	private static Object cloneObject(Object obj){
+        try{
+            Object clone = obj.getClass().newInstance();
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                if(field.get(obj) == null || Modifier.isFinal(field.getModifiers())){
+                    continue;
+                }
+                if(field.getType().isPrimitive() || field.getType().equals(String.class)
+                        || field.getType().getSuperclass().equals(Number.class)
+                        || field.getType().equals(Boolean.class)){
+                    field.set(clone, field.get(obj));
+                }else{
+                    Object childObj = field.get(obj);
+                    if(childObj == obj){
+                        field.set(clone, clone);
+                    }else{
+                        field.set(clone, cloneObject(field.get(obj)));
+                    }
+                }
+            }
+            return clone;
+        }catch(Exception e){
+            return null;
+        }
+    }
+	
 	public Refelection() {
 		super(Refelection.class.getClassLoader());
 	}
